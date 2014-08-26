c86336a964edc7815be7018064d94b616843bb9e
Adam Chlipala <adamc@csail.mit.edu>
2014-08-26 18:09:03 -0400

    Basic template

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ openshift-postgres
 ==================
 
 Basic Node.js + PostgreSQL template for OpenShift
+
+Replace this file with your project description!
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+var pg = require('pg');
+
+var connection_string = 'postgres://' + process.env.USER + '@localhost/mytest';
+
+if (process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD) {
+  connection_string = process.env.OPENSHIFT_POSTGRESQL_DB_USERNAME + ':' +
+        process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD + '@' +
+        process.env.OPENSHIFT_POSTGRESQL_DB_HOST + ':' +
+        process.env.OPENSHIFT_POSTGRESQL_DB_PORT + '/mypostgres';
+}
+
+var db = new pg.Client(connection_string);
+
+app.get('/', function(req, res) {
+    db.query('SELECT * FROM foo', function(err, result) {
+        if (err) {
+            return console.error('error running query');
+        }
+
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        for (var i = 0; i < result.rows.length; ++i)
+            res.write("#" + i + ": " + result.rows[i].baz + " / " + result.rows[i].bar + "\n");
+        res.end();
+    });
+});
+
+db.connect(function(err) {
+    if (err) {
+        return console.error('could not connect to database');
+    }
+
+    console.log('yay');
+
+    var port = process.env.OPENSHIFT_NODEJS_PORT;
+    var ip = process.env.OPENSHIFT_NODEJS_IP;
+
+    app.listen(port || 8080, ip);
+});
